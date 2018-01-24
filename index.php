4ec869464e1429b9374d91157ce2c0740184a64b
jayzie123 <34434591+jayzie123@users.noreply.github.com>
2018-01-24 13:57:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	
+</head>
+<body>
+	<img src ="web1.png" height = "300px" width="300px"/>
+ 	<img src ="web2.png" height = "300px" width="300px"/>
+ 	<img src ="1.png" height = "300px" width="300px"/>
+ 	<img src ="2.png" height = "300px" width="300px"/>
+ 	<img src ="3.png" height = "300px" width="300px"/>
+ 	<img src ="4.png" height = "300px" width="300px"/>
+ 	<img src ="5.png" height = "300px" width="300px"/>
+ 	<img src ="6.png" height = "300px" width="300px"/>
+ 	<img src ="7.png" height = "300px" width="300px"/>
+ 	<img src ="8.png" height = "300px" width="300px"/>
+ 	<img src ="9.png" height = "300px" width="300px"/>
+ 	<img src ="10.png" height = "300px" width="300px"/>
+ 	<img src ="11.png" height = "300px" width="300px"/>
+</body>
+</html>
